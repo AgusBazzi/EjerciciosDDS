02de57ffcea21e110151f7faa9f1eb58b6b1569f
CarolinaSeoane <carolina.b.seoane@gmail.com>
2021-06-19 17:55:07 -0300

    Agrego ejercicio 67

---
+++ 67-Emergencias/src/main/java/domain/Base.java
@@ -0,0 +1,9 @@
+package domain;
+
+public interface Base {
+
+    public int cantAmbulancias();
+    public int tiempoMedioAsistencia();
+    public int cantidadDeBasesSimples(); //Este no lo pide la consigna
+
+}
